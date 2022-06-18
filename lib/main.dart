6558d0696bbab0750aaa95c86f4b6ce949e4cd84
KaalaAadmi <51954157+KaalaAadmi@users.noreply.github.com>
2022-06-19 05:09:42 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +19,17 @@ class MyApp extends StatelessWidget {
     // var day="Tuesday";//just like vanilla js, this takes anything as data type.
     // const val=123;//this is how you declare constants.
     // final value=123;//this can be modified later.
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
