a2c17285823a7cad0337dbeff74a7db6689c4610
KaalaAadmi <51954157+KaalaAadmi@users.noreply.github.com>
2022-06-19 03:24:40 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-            child: Center(
-      child: Container(child: Text("Hello World!!")),
-    )));
+    // int days = 30;
+    // String name = "Kaalu";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp=30.5;//30. Takes both int and double values
+    // var day="Tuesday";//just like vanilla js, this takes anything as data type.
+    // const val=123;//this is how you declare constants.
+    // final value=123;//this can be modified later.
+    return MaterialApp(home: HomePage());
   }
 }
