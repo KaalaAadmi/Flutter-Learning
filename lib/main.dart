e0913b32e89abffe7b851f9cc94da1fe3ae3c649
KaalaAadmi <51954157+KaalaAadmi@users.noreply.github.com>
2022-07-08 02:18:10 +0530

    Day 9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -24,14 +25,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme(),
-          primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme(),
-          brightness: Brightness.dark),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: "/home",
       debugShowCheckedModeBanner: false,
       routes: {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Catalog App")),
+      appBar: AppBar(
+          title: Text(
+        "Catalog App",
+      )),
       body: Center(
         child:
             Container(child: Text("Welcome to $days days of flutter by $name")),
